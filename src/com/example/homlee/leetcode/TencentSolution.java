72264603abab65c470f883d34d4230c6173b5217
Leo.Yuan <hlyu@royole.com>
2020-08-14 18:05:00 +0800

    知识整理

--- src/com/example/homlee/leetcode/TencentSolution.java
+++ src/com/example/homlee/leetcode/TencentSolution.java
@@ -2,5 +2,24 @@ package com.example.homlee.leetcode;
 
 public class TencentSolution {
 
+    public int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
 
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[i] != nums[j]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+        TencentSolution solution = new TencentSolution();
+        int[] nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        System.out.println("removeDuplicates " + solution.removeDuplicates(nums));
+    }
 }
